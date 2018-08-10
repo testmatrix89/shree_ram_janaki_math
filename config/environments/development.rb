778d26ce8a74fb4cd1f013a80a443086a1b33f6c
Prince Kumar <testmatrix89@gmail.com>
2018-08-11 00:27:04 +0530

    updated

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,6 +44,10 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
+
+  # Do not fallback to assets pipeline if a precompiled asset is missed.
+  config.assets.compile = true
+
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
