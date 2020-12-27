eb35eebf46957a74b3d01c4d45013f96a1159ee9
Prince Kumar <testmatrix89@gmail.com>
2020-12-27 18:49:09 +0530

    updated

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -2,8 +2,8 @@ class AlbumsController < ApplicationController
   def index
     images_dir = Dir.glob("app/assets/images/*.jpg")
     images_dir = images_dir.map{|img| img = img.split('/').last }
-    gallery_dir = Dir.glob("app/assets/images/gallery/*")
-    gallery_dir = gallery_dir.map{|img| img = "gallery/#{img.split('/').last}" }
-    @images = gallery_dir.concat(images_dir)
+    # gallery_dir = Dir.glob("app/assets/images/gallery/*")
+    # gallery_dir = gallery_dir.map{|img| img = "gallery/#{img.split('/').last}" }
+    @images = images_dir # gallery_dir.concat(images_dir)
   end
 end
