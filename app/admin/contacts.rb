a04133d805eae7337b0560d319ce6781e59706b6
Prince Kumar <testmatrix89@gmail.com>
2021-01-10 22:43:24 +0530

    updated

---
+++ app/admin/contacts.rb
@@ -0,0 +1,27 @@
+ActiveAdmin.register Contact do
+  controller do
+    actions :all, :except => [:edit, :new]
+  end
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # permit_params :list, :of, :attributes, :on, :model
+  permit_params :email, :name, :body
+  # or
+  #
+  # permit_params do
+  #   permitted = [:permitted, :attributes]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+
+  index do
+    selectable_column
+    column 'Email', :email
+    column 'name', :name
+    column 'body', :body
+    column 'Created Date', :created_at
+    column 'Updated Date', :updated_at
+    actions
+  end
+end
--- app/admin/subscribers.rb
+++ app/admin/subscribers.rb
@@ -1,4 +1,8 @@
 ActiveAdmin.register Subscriber do
+
+	controller do
+  	actions :all, :except => [:edit, :new]
+  end
 	# See permitted parameters documentation:
 	# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 	#
